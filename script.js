fcb358e96af4ec0f3fc529a305164bf5c3c3a9a0
Abhishek kumar chaudhary <avioncompany00@gmail.com>
2025-01-07 14:11:53 +0545

    new update slider

--- script.js
+++ script.js
@@ -132,3 +132,29 @@ document.querySelectorAll(".faq-item .question").forEach((question) => {
 
 
 
+
+
+// swipper for 
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
